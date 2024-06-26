24b0173edd1c7eb143f8ca9be9e3ed625071c898
tludwig <admin@tludwig.dev>
2024-06-26 18:16:22 +0200

    feat: add json parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 #include <WebSocketsClient.h>
+#include <iostream>
+#include <string>
+#include <ArduinoJson.h>
+
 #include "credentials.cpp"
 #include "wifi.cpp"
 #include "Keyboard.h"
@@ -15,11 +19,36 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 		case WStype_CONNECTED:
 			Serial.printf("[WSc] Connected to url: %s\n", payload);
 
-			// send message to server when Connected
+			// send message to server when Connectedc
 			webSocket.sendTXT("Connected");
 			break;
 		case WStype_TEXT:
 			Serial.printf("[WSc] get text: %s\n", payload);
+
+  			StaticJsonDocument<200> doc;
+
+			char json[] = payload;
+
+			if (error) {
+				Serial.print(F("deserializeJson() failed: "));
+				Serial.println(error.f_str());
+				return;
+			}
+
+			// Fetch values.
+			//
+			// Most of the time, you can rely on the implicit casts.
+			// In other case, you can do doc["time"].as<long>();
+			const char* sensor = doc["sensor"];
+			long time = doc["time"];
+			double latitude = doc["data"][0];
+			double longitude = doc["data"][1];
+
+			// Print values.
+			Serial.println(sensor);
+			Serial.println(time);
+			Serial.println(latitude, 6);
+			Serial.println(longitude, 6);
 
 			// send message to server
 			// webSocket.sendTXT("message here");
@@ -54,7 +83,7 @@ void setup() {
 	webSocket.onEvent(webSocketEvent);
 
     // Connect Websocket
-    webSocket.begin(Credentials::websocket_ip, Credentials::websocket_port, "/")
+    webSocket.begin(Credentials::websocket_ip, Credentials::websocket_port, "/");
 
 	// try ever 5000 again if connection has failed
 	webSocket.setReconnectInterval(5000)
