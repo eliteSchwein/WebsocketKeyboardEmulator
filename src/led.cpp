cbedcbf1402031da3af0a2b1c50928ce751e8bb0
tludwig <business@tludwig.dev>
2024-09-26 00:06:00 +0200

    feat: add led functions for build in led feat: add bootsel reset function

---
+++ src/led.cpp
@@ -0,0 +1,52 @@
+// Created by tludwig on 25.09.24.
+
+#include <Arduino.h>
+
+class LedHandler {
+    bool LedBlink = false;
+
+public:
+    void init() {
+        Serial.println("[LED] Init LED ");
+        pinMode(LED_BUILTIN, OUTPUT);
+        turnOn();
+    }
+
+    void turnOn() {
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    void turnOff() {
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    void blink() {
+        digitalWrite(LED_BUILTIN, LOW);
+        sleep_ms(250);
+        digitalWrite(LED_BUILTIN, HIGH);
+    }
+
+    void turnBlinkOn() {
+        if(LedBlink) {
+            return;
+        }
+        LedBlink = true;
+        turnOff();
+    }
+
+    void turnBlinkOff() {
+        if(!LedBlink) {
+            return;
+        }
+        LedBlink = false;
+        turnOn();
+    }
+
+    void handleBlink() {
+        if(!LedBlink) {
+            return;
+        }
+        blink();
+        sleep_ms(1000);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <Arduino.h>
 #include <WebSocketsClient.h>
-#include <iostream>
-#include <string>
 #include <ArduinoJson.h>
-#include <cstdint>
 
 #include "credentials.cpp"
 #include "wifi.cpp"
 #include "keyboard.cpp"
+#include "led.cpp"
 
 WiFiHandler wifi;
 KeyboardHandler keyboard;
 WebSocketsClient webSocket;
+LedHandler led;
 
 void webSocketEvent(WStype_t type, unsigned char * payload, unsigned length) {
 	switch(type) {
 		case WStype_DISCONNECTED:
 			Serial.println("[WS] Disconnected!");
+			led.turnOff();
 			break;
 		case WStype_CONNECTED:
 			Serial.print("[WS] Connected to ");
@@ -24,6 +24,7 @@ void webSocketEvent(WStype_t type, unsigned char * payload, unsigned length) {
 			Serial.print(":");
 			Serial.print(Credentials::websocket_port);
 			Serial.println("");
+			led.turnOn();
 			break;
 		case WStype_TEXT:
             const char* jsonPayload = (const char*)payload;
@@ -36,6 +37,7 @@ void webSocketEvent(WStype_t type, unsigned char * payload, unsigned length) {
             const int duration = doc["data"]["duration"];
 
             if(method == "ping_keyboard") {
+            	led.blink();
 				webSocket.sendTXT("{\"keyboard_status\": \"online\"}");
               	break;
             }
@@ -48,21 +50,29 @@ void webSocketEvent(WStype_t type, unsigned char * payload, unsigned length) {
 				break;
 			}
 
+			led.blink();
+
 			JsonArray keys = doc["data"]["keys"];
-			
+
 			keyboard.triggerKeys(keys, duration);
+			led.blink();
 			webSocket.sendTXT("{\"keyboard_status\": \"finished\"}");
 
 			break;
 	}
-
 }
 
 void setup() {
     Serial.begin(115200);
 
     Serial.println("");
-    Serial.println("[SYS] Startup WebsocketKeyboardEmulator 0.0.1...");
+	Serial.println("[SYS] Startup WebsocketKeyboardEmulator 0.0.2...");
+
+	led.init();
+	multicore_reset_core1();
+	multicore_launch_core1(loop1);
+
+	led.turnBlinkOn();
 
     wifi.wifiSetup(Credentials::wifi_ssid, Credentials::wifi_password);
     
@@ -80,8 +90,24 @@ void setup() {
     webSocket.begin(Credentials::websocket_ip, Credentials::websocket_port);
 
 	webSocket.setReconnectInterval(1000);
+
+	led.turnBlinkOff();
 }
 
 void loop() {
 	webSocket.loop();
+}
+
+void loop1() {
+	while(true) {
+		led.handleBlink();
+		if (BOOTSEL) {
+			Serial.println("[SYS] Bootsel pressed!");
+			Serial.println("[SYS] Hold 5 seconds for flash mode, release for just reboot!");
+			Serial.println("[SYS] Reboot in 3 seconds!");
+			sleep_ms(3000);
+			rp2040.reboot();
+		}
+        sleep_ms(250);
+	}
 }
